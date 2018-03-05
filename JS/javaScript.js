a0ca7f773342c0a7ded17f04593dffb200bfbd3a
Yura <onyfrak1999@ukr.net>
2018-03-05 17:19:43 +0200

    Second commit. Can play but is something error

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -1,3 +1,82 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+
+var new_game = new Game();
+new_game.init();
+	
+		
+});
+
+
+function Resize(){
+	
+		var height = window.innerHeight;	
+		var width = window.innerWidth;	 
+		//alert(height + " " + width);
+		//var  element  = document.getElementsByClassName("screen")[0].style.height = height + "px";    
+}
+
+
+var  index_user_stone = null; 
+
+function User_schoose(obj){
+
+	var game = new Create_game();
+	var index = game.Search_who_move();
+	index_user_stone = obj;
+
+	//if (index == 0 ) {
+
+		if ((index_user_stone)!=null) {
+			
+			current_stone = user.user_domino[index_user_stone];
+			
+			if(current_stone != undefined){
+				game.Some_function(0,current_stone);
+				index_user_stone = null;
+				Load_page(user.user_domino);
+				Add_stone_to_left_field();
+				Add_stone_to_right_field();
+
+			}
+			
+		}		
+	
+};
+
+
+function Play_process(){
+	var game = new Create_game();
+
+		for (var index = 1; index < 4; index++) {
+
+			if (game_status==true) {
+			
+				var current_stone = array_player[index].Search_need_stone(left_end, right_end,
+													    array_player[index].Get_array_stone(), array_move,index);
+				//console.log(current_stone.left_side + "  " + current_stone.right_side);
+
+				if(current_stone == undefined){
+					return;
+													
+				}
+				else{
+					game.Some_function(index, current_stone);	
+
+					Add_stone_to_left_field();
+					Add_stone_to_right_field();									
+
+				}			
+								
+			}
+			
+		}			
+
+};
+
+
+
+
 var has_to_restart=false;
 var game_status = true;
 
@@ -16,8 +95,7 @@ var right_part_field = new Array();
 
 var left_end=0;
 var right_end=0;
-
-
+var index_first_move;
 
 
 class  Player{
@@ -52,32 +130,28 @@ class  Player{
 		var current_array = new Array();
 
 		for (var i = 0; i < array.length; i++) {
-			if(array[i].left_side ==number_right || array[i].right_side == number_right){
+			 if( array[i].left_side == number_right || array[i].right_side == number_right || 
+				 array[i].left_side == number_left || array[i].right_side == number_left){
 				current_array.push(array[i]);
 				
 			}
 		}	
-		var current_stone = this.Get_stone(current_array);		
-		console.log("Go " + current_stone.right_side + " " + current_stone.left_side);
+		var current_stone = this.Get_stone(current_array);	
+
+		if(current_stone == undefined ){
+			//console.log("undefined");
+
+		}
+		else{
+			//console.log("Go " + current_stone.right_side + " " + current_stone.left_side);
+		}	
+		
         
         return current_stone;
 
 	};
 
-	Next_player(number_left,number_right,array, array_move,index){
-		
-			//if (array_move[index] == true)
-			 {				
-				var current_stone = this.Search_need_stone(number_left,number_right,array);
-				array_move[index+1]=true;
-			}
-
-			return 	current_stone;
-
-	}
-
-
-}
+};
 
 
 class User extends Player{
@@ -90,8 +164,7 @@ class User extends Player{
 
 	Get_array_stone(){
 		return this.user_domino;
-	}
-
+	};
 
 };
 
@@ -162,8 +235,7 @@ class Create_game{
 					//console.log(stone[index].right_side + " " + stone[index].left_side);
 					index++;			
 				}
-
-				//console.log(" ");			
+						
 			}
 
 	};
@@ -180,68 +252,74 @@ class Create_game{
 
 	};
 
+	Remove(array, element) {
+    var index = array.indexOf(element);
+    array.splice(index, 1);
+	}
+
 	Play(){
 
 		var index = this.Search_who_move();
-		var i = 2;
+		this.Play_process(index);		
 		
-		for (var i = 0; i < 4; i++) {
-			
-			if (index <3) {
-						var current_stone = array_player[index].Next_player(left_end, right_end,
-										    array_player[index].Get_array_stone(), array_move,index);
-						
+	};
 
-						if(left_part_field[left_part_field.length-1].left_side == current_stone.left_side){
-								this.Reverse_stone(current_stone);
-								left_part_field.push(current_stone);
-								array_player[index].Get_array_stone().splice(current_stone);
-								left_end = current_stone.left_side;
+	Some_function( index, current_stone){
 
-						}
-						else if(left_part_field[left_part_field.length-1].left_side == current_stone.right_side){
-								left_part_field.push(current_stone);
-								left_end = current_stone.left_side;
-								array_player[index].Get_array_stone().remove(current_stone);
-
-						}
-						else if(right_part_field[right_part_field.length-1].right_side == current_stone.right_side){
-							this.Reverse_stone(current_stone);
-							right_part_field.push(current_stone);
-							right_end = current_stone.right_side;
-							array_player[index].Get_array_stone().remove(current_stone);
-
-						}
-						else if(right_part_field[right_part_field.length-1].right_side == current_stone.left){
-							right_part_field.push(current_stone);
-							right_end = current_stone.right_side;
-							array_player[index].Get_array_stone().remove(current_stone);
-
-						}
-				index++;
-			}
-			else{
-				
-				this.Play();
-			}
-
-			//i--;
+		if(left_part_field[left_part_field.length-1].left_side == current_stone.left_side){
+			this.Reverse_stone(current_stone);
+			left_part_field.push(current_stone);
+			this.Remove(array_player[index].Get_array_stone(),current_stone);
+			left_end = current_stone.left_side;
+			//console.log(left_end);
 
 		}
-					
-				
+		else if(right_part_field[right_part_field.length-1].right_side == current_stone.left_side){
+			right_part_field.push(current_stone);
+			right_end = current_stone.right_side;  
+			this.Remove(array_player[index].Get_array_stone(),current_stone);			
+			//console.log(right_end);
+		}
+		else if(left_part_field[left_part_field.length-1].left_side == current_stone.right_side){
+			left_part_field.push(current_stone);
+			left_end = current_stone.left_side;
+			this.Remove(array_player[index].Get_array_stone(),current_stone);
+			//console.log(left_end);
 
+		}
+		// else if(right_part_field[right_part_field.length-1].right_side == current_stone.right_side){
+		else if(right_part_field[right_part_field.length-1].right_side == current_stone.right_side){
+			this.Reverse_stone(current_stone);
+			right_part_field.push(current_stone);
+			right_end = current_stone.right_side;
+			this.Remove(array_player[index].Get_array_stone(),current_stone);
+	 		//console.log(right_end);
+		}
 		
+		if(array_player[index].Get_array_stone().length == 0)
+		{
+			game_status = false;
+			console.log("gameOver");
+			console.log(array_player[index]);
+
+			for (var i = 1; i < left_part_field.length; i++) {
+					console.log(left_part_field[i]);
+			}
+			for (var i = 0; i < right_part_field.length; i++) {
+					console.log(right_part_field[i]);
+				}
+			}
 
 	};
 
+	
 	Result(){
 		for (var i = 0; i < array_player.length; i++) {
 				array_player[i].Display_player_stones(array_player[i].Get_array_stone());	
 		}
-	}
+	};
 
-	shuffle(a) {
+	Shuffle(a) {
 	    var j, x, i;
 	    for (i = a.length - 1; i > 0; i--) {
 	        j = Math.floor(Math.random() * (i + 1));
@@ -253,10 +331,8 @@ class Create_game{
 
 
 	Domino_distribution_result(){
-		this.shuffle(stone);
-		this.shuffle(stone);
-
-
+		this.Shuffle(stone);
+		this.Shuffle(stone);
 		
 	};	
 
@@ -287,11 +363,7 @@ class Create_game{
 
 	};
 
-	Add_stone_to_field(array, stone){
-
-		array.push(stone);
-
-	};
+	
 
 
 	Search_who_move(){
@@ -303,29 +375,31 @@ class Create_game{
 			move_index= 3;
 			left_part_field.push(first_stone);
 			right_part_field.push(first_stone);
-			// console.log("user.user_move"+ left_part_field[0].left_side +left_part_field[0].right_side);
+			this.Remove(user.user_domino, first_stone);
+			
 		}
 		else if (this.Search_the_smallest_double(first_computer.computer_first_domino)) {
-			first_computer.computer_first_move =true;
-			
+			first_computer.computer_first_move =true;			
 			move_index= 2;
 			left_part_field.push(first_stone);
 			right_part_field.push(first_stone);
-			//console.log("computer_first_move" + left_part_field[0].left_side);
+			this.Remove(first_computer.computer_first_domino, first_stone);
+			
 		}
 		else if (this.Search_the_smallest_double(second_computer.computer_second_domino)) {
 			second_computer.computer_second_move =true;
-			move_index= 3;
-			//console.log("computer_second_move");
+			move_index= 3;			
 			left_part_field.push(first_stone);
 			right_part_field.push(first_stone);
+			this.Remove(second_computer.computer_second_domino, first_stone);
 		}
-		else if (this.Search_the_smallest_double(third_computer.computer_third_domino)) {
+		else if (this.Search_the_smallest_double(third_computer.computer_third_domino)) 
+		{
 			third_computer.computer_third_move=true;
-			move_index= 0;
-			//console.log("computer_third_move");
+			move_index= 0;			
 			left_part_field.push(first_stone);
 			right_part_field.push(first_stone);
+			this.Remove(third_computer.computer_third_domino, first_stone);
 		}
 
 		array_move.push(user.user_move);
@@ -346,26 +420,120 @@ class Create_game{
 	};
 }
 
-function Load_page(){
+function Load_page(array){
 
-	for (var i = 1; i < (user.user_domino.length)*2 +1; i++) {
+	for (var i = 1; i < (7)*2 +1; i++) {
 		if (i%2!=0) {
 
 			var image_first_part = document.getElementById("my"+i.toString());
-			image_first_part.src = Get_image_source(user.user_domino[parseInt((i-1)/2)].left_side); 
+			if(array[parseInt((i-1)/2)] != undefined){
+				image_first_part.src = Get_image_source(array[parseInt((i-1)/2)].left_side);
+			}
+			else{
+				image_first_part.src = "Resource/Empty.png";
+				image_first_part = document.getElementById("my"+(i+1).toString());
+				image_first_part.src = "Resource/Empty.png";
+
+			}			 
 			
 		}
 		else{
+			if(array[parseInt((i-1)/2)] != undefined){
 			var image_second_part = document.getElementById("my"+(i).toString());
-			image_second_part.src = Get_image_source(user.user_domino[parseInt((i-1)/2)].right_side); 
+			image_second_part.src = Get_image_source(array[parseInt((i-1)/2)].right_side); 
+			}								
+		}
+	}
+
+		
+		
+	
+	 
+};
+
+
+
+function Create_stone(i, part_field){
+	//if (i<29)
+	 {
+		if (i%2!=0) {
+
+			var image_first_part = document.getElementById("l"+i.toString());
+			if (part_field[parseInt((i-1)/2)] != undefined) {
+				if (part_field[parseInt((i-1)/2)].left_side != null) {				
+				image_first_part.src = Get_image_source(part_field[parseInt((i-1)/2)].left_side);
+				
+				}
+			}			 
+			
+		}
+		else{
+			var image_second_part = document.getElementById("l" +(i).toString());
+			if (part_field[parseInt((i-1)/2)] != undefined) {
+				if (part_field[parseInt((i-1)/2)].right_side != null) {
+				image_second_part.src = Get_image_source(part_field[parseInt((i-1)/2)].right_side);
+				}
+			}		 
 			
 		}
 
+	}
+	// else{
+
+
+	// }
+}
+
+function Add_stone_to_left_field(){
+
+	for (var i = 1; i < 57; i++) {
+		Create_stone(i, left_part_field);
+		
+	}
+
+};
+
+function Add_stone_to_right_field(){
+
+	var t = 1;
+
+	for (var i = 56; i >0; i--) {
+		if (i%2 ==0) {
+
+			var image_first_part = document.getElementById("l"+(i).toString());
+			if (right_part_field[t] != undefined) {
+				if (right_part_field[t].left_side != null) {				
+				image_first_part.src = Get_image_source(right_part_field[t].left_side);
+				
+				}
+			}
+						 
+			
+		}
+	else{
+			var image_second_part = document.getElementById("l" +(i).toString());
+			if (right_part_field[t] != undefined) {
+				if (right_part_field[t].right_side != null) {
+				image_second_part.src = Get_image_source(right_part_field[t].right_side);
+				}
+			}
+
+			t++;	 
+			
+		}
 		
 		
 	}
 
-	 
+};
+
+class Stone {
+  constructor(left_side, right_side) {
+    this.left_side = left_side;
+    this.right_side = right_side;
+  }
+  
+ 
 };
 
 function Get_image_source(number){
@@ -410,36 +578,49 @@ class Game{
 		new_game.Create_domino_array();
 		new_game.Domino_distribution_result();
 		new_game.Distribution_domino_to_users();
-		new_game.Search_who_move();
-		
-		new_game.Play();
-		Load_page();
-		//new_game.Result();
-		
-		
-			
+		index_first_move = (new_game.Search_who_move());
+		Load_page(user.user_domino);
+		First_go();			
 
 	}
+	
+};
 
-	start(){
+function First_go(){
 
+	var game = new Create_game();
+	
+
+	
+
+	if (game_status == true && index_first_move==0) {
+		//if () {
+			alert("User go");
+		//}
+	}
+	else{
+
+		for (var i = index_first_move; i < 4; i++) {
+				
+				
+					var current_stone = array_player[i].Search_need_stone(left_end, right_end,
+														    array_player[i].Get_array_stone(), array_move,i);
+					//console.log(current_stone.left_side + "  " + current_stone.right_side);
+
+					if(current_stone == undefined){
+						return;
+														
+					}
+					else{
+						game.Some_function(i, current_stone);
+						Add_stone_to_left_field();
+						Add_stone_to_right_field();		
+					}
+
+			alert("User go  c");		
+
+		}
 	}
 
+
 };
-
-
-class Stone {
-  constructor(left_side, right_side) {
-    this.left_side = left_side;
-    this.right_side = right_side;
-  }
-  
- 
-};
-
-var game= new Game();
-game.init();
-
-
-
-
